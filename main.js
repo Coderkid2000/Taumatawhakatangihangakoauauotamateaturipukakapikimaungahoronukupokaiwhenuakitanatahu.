9e73f9012593721db0036b3b7ab00317751c4318
Coderkid2000 <114805555+Coderkid2000@users.noreply.github.com>
2023-05-31 20:45:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot()
 
 console.log('ml5 version:' , ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lCjq0lEvF/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NrSb6F76M/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('model loaded')
